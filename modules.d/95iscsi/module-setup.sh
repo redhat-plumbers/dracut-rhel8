961ba7777c8646cc9f27e54d59f113ab538fba97
Lukáš Nykrýn <lnykryn@redhat.com>
2022-01-21 16:09:19 +0100

    Update modules.d/95iscsi/module-setup.sh
    Co-authored-by: Pavel Valena <pvalena@redhat.com>

--- modules.d/95iscsi/module-setup.sh
+++ modules.d/95iscsi/module-setup.sh
@@ -126,7 +126,7 @@ install_iscsiroot() {
         "/etc/sysconfig/network-scripts/ifcfg-$ifname" \
         "/etc/sysconfig/network/ifcfg-$ifname" \
     ; do
-        [ -f "$_path" ] && bootproto=$(sed -n "s/BOOTPROTO='\?\([[:alpha:]]*6\?\)4\?/\1/p" "$_path")
+        [ -r "$_path" ] && bootproto=$(sed -n "s/BOOTPROTO='\?\([[:alpha:]]*6\?\)4\?/\1/p" "$_path")
     done
 
     if [ $bootproto ]; then
